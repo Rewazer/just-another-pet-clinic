7750f1c8c62f8e1049b5d7d3c12cff67acad62a0
Rewazer <runner.px@gmail.com>
2020-05-14 22:41:07 +0500

    Initial project Add

---
+++ src/main/java/ru/hcc/petclinic/JustAnotherPetClinicApplication.java
@@ -0,0 +1,13 @@
+package ru.hcc.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustAnotherPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustAnotherPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hcc/petclinic/JustAnotherPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hcc.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustAnotherPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
