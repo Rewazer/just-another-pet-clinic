286e7dd1b89959312476a28bfcefdf7ac0d18fa5
Rewazer <runner.px@gmail.com>
2020-05-25 19:08:42 +0500

    Create PetType Map Service Implementation, closes #28

---
+++ pet-clinic-data/src/main/java/ru/hcc/petclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+/*
+ * Created by SS on 25/05/2020
+ */
+
+package ru.hcc.petclinic.services;
+
+import ru.hcc.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
