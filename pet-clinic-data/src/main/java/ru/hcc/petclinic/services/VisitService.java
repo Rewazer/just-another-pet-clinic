e66af19db26ae3d24a073740c7d38ec3df3a794a
Rewazer <runner.px@gmail.com>
2020-05-29 01:48:55 +0500

    Create Visit Map Service, closes #45

---
+++ pet-clinic-data/src/main/java/ru/hcc/petclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+/*
+ * Created by SS on 29/05/2020
+ */
+
+package ru.hcc.petclinic.services;
+
+import ru.hcc.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
