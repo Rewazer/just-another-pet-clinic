d961b1522fdf8808be3fb1c89abc4f33854bb635
Rewazer <runner.px@gmail.com>
2020-05-29 00:28:40 +0500

    Create Visit Entity, closes #37

--- pet-clinic-data/src/main/java/ru/hcc/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/ru/hcc/petclinic/model/Visit.java
@@ -4,11 +4,21 @@
 
 package ru.hcc.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+
+    @Column(name = "name")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
