48f34959fcd4043b249ebfae9ebc1a3381e783e8
Rewazer <runner.px@gmail.com>
2020-05-29 12:36:12 +0500

    Add Project Lombok and Refactor, closes #50

--- pet-clinic-data/src/main/java/ru/hcc/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/ru/hcc/petclinic/model/PetType.java
@@ -1,19 +1,18 @@
 package ru.hcc.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
